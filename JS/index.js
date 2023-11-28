722bb1a116572123b8b6280f69fd68198d151a22
Nicolassackmann <nicosack99@gmail.com>
2023-11-27 22:49:29 -0300

    first commit

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+
+let nombre = prompt("ingrese su nombre");
+console.log("El nombre ingresado es: " + nombre);
+
+let edad = parseInt( prompt ("Ingrese su Edad") );
+console.log("La edad ingresada es: " + edad);
+
+let numeroDecimalFav = parseFloat(prompt("Ingrese su numero decimal favorito"));
+console.log("Su numero decimal favorito es: " + numeroDecimalFav);
